cdd55119d6d063a5b96a74405594e10b6de462e0
rmarasigan <russianhielle@gmail.com>
2022-07-22 10:19:41 +0800

    ADD: env configuration for account and region

--- bin/todo-list.ts
+++ bin/todo-list.ts
@@ -4,4 +4,9 @@ import * as cdk from 'aws-cdk-lib';
 import { TodoListAppStack } from '../lib/stacks/todo-list-stack';
 
 const app = new cdk.App();
-new TodoListAppStack(app, 'TodoListAppStack');
+new TodoListAppStack(app, 'TodoListAppStack', {
+   env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION
+   }
+});
