8f61c091c9e2f7a051a49ee5fdcbddb35d1b6465
rmarasigan <russianhielle@gmail.com>
2023-01-15 15:53:34 +0800

    Revert "FIX: Bootstrap JS import"
    This reverts commit 171d8603797705c9bc28dd0049347346343dafa0.

--- bin/todo-list.ts
+++ bin/todo-list.ts
@@ -7,6 +7,6 @@ const app = new cdk.App();
 new TodoListAppStack(app, 'TodoListAppStack', {
    env: {
       account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: 'ap-southeast-1'
+      region: process.env.CDK_DEFAULT_REGION
    }
 });
